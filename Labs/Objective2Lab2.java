ef0fb05fbafd428cafc5e7bc1f8b2bb9b794b38d
Desiree <desiree.vanassche@gmail.com>
2022-01-21 15:55:43 -0700

    Still need to correct for spacing or in this case lack of spacing

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,21 @@
+public class Objective2Lab2 {
+
+  public static void main(String[] args) {
+
+    String name = "Desiree VanAssche";
+    int age = 28;
+    String state = "Michigan";
+
+System.out.print("Name:");
+System.out.print(name );
+
+
+System.out.print("Age:");
+System.out.print(age );
+
+
+System.out.print("State:");
+System.out.print(state );
+
+  }
+}
