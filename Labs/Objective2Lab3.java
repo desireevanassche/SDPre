dd6ca2c6690d9ee12e41c1590de10ab3023585d5
Desiree <desiree.vanassche@gmail.com>
2022-01-21 16:00:42 -0700

    Objective2Lab3 Complete

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,15 @@
+public class Objective2Lab3 {
+
+  public static void main(String[] args) {
+
+    double num1, num2, num3, average;
+
+    num1 = 16;
+    num2 = 8;
+    num3 = 2;
+    average = (num1 + num2 + num3) / 3;
+
+System.out.println(average);
+
+  }
+}
