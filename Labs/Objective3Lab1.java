0ab6d0f71108dba0cfe4db2d658305b2d4bc3987
Desiree <desiree.vanassche@gmail.com>
2022-01-21 16:07:06 -0700

    This lab is a duplicate of LObjective2Lab3 but practice makes perfect.

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,15 @@
+class Objective3Lab1 {
+
+  public static void main(String[] args) {
+
+    double num1, num2, num3, average;
+
+    num1 = 44;
+    num2 = 99;
+    num3 = 16.9876;
+
+    average = (num1+num2+num3) / 3;
+
+    System.out.println(average);
+  }
+}
