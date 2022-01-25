80d20140ce5c58b02bd800c320b07d7a715f5d11
Desiree <desiree.vanassche@gmail.com>
2022-01-25 08:12:07 -0700

    Still need to complete code but it is functional

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+
+  public static void main(String[] args) {
+
+Scanner keyboard = new Scanner(System.in);
+
+
+System.out.println("What is your first name?");
+String fname = keyboard.nextLine();
+
+
+
+System.out.println("Hello " + fname +"!");
+
+  }
+}
