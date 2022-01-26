5ba7ad3783573dcd033350b168cf36699e4af2e3
Desiree <desiree.vanassche@gmail.com>
2022-01-26 12:18:20 -0700

    Objective7Lab3 Completed.

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,21 @@
+public class Objective7Lab3 {
+
+  public static void main(String[] args){
+
+    int counter = 1;
+
+    while(counter <= 20) {
+      System.out.print(counter);
+      counter = counter + 1;
+
+      if (counter % 2 == 0) {
+        System.out.println(" is odd.");
+    }
+
+    if (counter % 2 != 0) {
+      System.out.println(" is even.");
+    }
+      }
+
+  }
+}
