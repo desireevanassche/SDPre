ef5d24c9453866cf6d8df9a059b32c88392998ec
Desiree <desiree.vanassche@gmail.com>
2022-01-27 11:01:30 -0700

    Objective7Lab4 Complete.

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,17 @@
+public class Objective7Lab4 {
+
+  public static void main(String[] args) {
+
+    int counter = 1;
+    int sum = 1;
+
+
+    while (counter <= 20) {
+      sum = sum + counter;
+      counter = counter + 1;
+      }
+//put the println on the outside of the curly braces for sum without counter
+      System.out.println(sum);
+
+  }
+}
