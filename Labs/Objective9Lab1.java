c3e24fd25f096310e7875fb2c131c85f4d04767e
Desiree <desiree.vanassche@gmail.com>
2022-01-27 14:07:22 -0700

    Objective9Lab1 Completed.

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,12 @@
+public class Objective9Lab1 {
+
+  public static void main(String[] args) {
+    printName();
+  //line 5 invokes or calls the method
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+    //printName method is defined above
+  }
+}
