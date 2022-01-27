80374b914a9eda90afb28c6a6410ec926b51778d
Desiree <desiree.vanassche@gmail.com>
2022-01-27 13:09:02 -0700

    Objective8Lab1 Complete.

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+
+  public static void main(String[] args) {
+
+    for (int int1=1; int1<=10; int1+=1){
+      System.out.println(int1);
+      //counts from 1 to 10
+    }
+
+    for (int int2=10; int2>=1; int2-=1){
+      System.out.println(int2);
+      //counts from 10 to 1
+    }
+  }
+}
